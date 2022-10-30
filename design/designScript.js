1d93de95e2a9e64eb52a1b0c7dd39e8de636daaa
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2022-10-30 15:49:51 -0300

    full project

---
+++ design/designScript.js
@@ -0,0 +1,65 @@
+const markAllasRead = document.getElementById('mark-all-as-read');
+const numberOfNotifications = document.getElementById('number-of-notification');
+const divNotification = document.getElementById('div-notification');
+const point = document.getElementById('point');
+
+const divNotification2 = document.getElementById('div-notification2');
+const point2 = document.getElementById('point2');
+
+const divNotification3 = document.getElementById('div-notification3');
+const point3 = document.getElementById('point3');
+
+const divNotification4 = document.getElementById('div-notification4');
+const point4 = document.getElementById('point4');
+
+const divNotification5 = document.getElementById('div-notification5');
+const point5 = document.getElementById('point5');
+
+const divNotification6 = document.getElementById('div-notification6');
+const point6 = document.getElementById('point6');
+
+const divNotification7 = document.getElementById('div-notification7');
+const point7 = document.getElementById('point7');
+
+const divNotification8 = document.getElementById('div-notification8');
+const point8 = document.getElementById('point8');
+
+
+
+function markAsRead() {
+
+  numberOfNotifications.textContent = '0'
+
+  divNotification.style.backgroundColor = 'white';
+  divNotification.style.border = 'none';
+  point.style.display = 'none';
+
+  divNotification2.style.backgroundColor = 'white';
+  divNotification2.style.border = 'none';
+  point2.style.display = 'none';
+
+  divNotification3.style.backgroundColor = 'white';
+  divNotification3.style.border = 'none';
+  point3.style.display = 'none';
+
+  divNotification4.style.backgroundColor = 'white';
+  divNotification4.style.border = 'none';
+  point4.style.display = 'none';
+
+  divNotification5.style.backgroundColor = 'white';
+  divNotification5.style.border = 'none';
+  point5.style.display = 'none';
+
+  divNotification6.style.backgroundColor = 'white';
+  divNotification6.style.border = 'none';
+  point6.style.display = 'none';
+
+  divNotification7.style.backgroundColor = 'white';
+  divNotification7.style.border = 'none';
+  point7.style.display = 'none';
+
+  divNotification8.style.backgroundColor = 'white';
+  divNotification8.style.border = 'none';
+  point8.style.display = 'none';
+
+}
